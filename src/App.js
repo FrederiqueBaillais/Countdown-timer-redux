02b0b4f7d7bb5718a48a4a7032f443b49b75ff54
Laura-VLN <lauravilain.pro@gmail.com>
2021-07-06 16:22:17 +0200

    scss added

--- src/App.js
+++ src/App.js
@@ -1,12 +1,11 @@
 import React from "react";
+import './assets/css/style.css';
 import Timer from "./containers/Timer/Timer";
 
 function App() {
 	return (
-		<div>
-			<h1 className="text-5xl md:text-4xl text-center font-thin pt-8 px-6">
-				Redux Stopwatch
-			</h1>
+		<div className="content">
+			<h1>Redux Stopwatch</h1>
 			<Timer />
 		</div>
 	);
